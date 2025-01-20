1749a9f6e944f650b5d4257093045b050c11b53e
a85277 <148544600+a85277@users.noreply.github.com>
2025-01-20 19:07:42 +0000

    Update io_ficheiros.py

--- src/io_ficheiros.py
+++ src/io_ficheiros.py
@@ -1,59 +1,56 @@
 import pickle
 
-from clientes import nome_ficheiro_lista_de_clientes
-from faturas import nome_ficheiro_lista_de_faturas
+from emprestimos import nome_ficheiro_lista_de_emprestimos
+from funcionarios import nome_ficheiro_lista_de_funcionarios
+from leitores import nome_ficheiro_lista_de_leitores
+from livros import nome_ficheiro_lista_de_livros
 
-from veiculos import nome_ficheiro_lista_de_veiculos
 
-
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
-
-# ...
-
+ficheiros com o nome io_ficheiros*.py na pasta temp e faça um commit de cada vez
+Quando este ficheiro estiver completo com todas as suas funções,
+deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
+todos os outros ficheiros temp/io_ficheiros-*.py, e inclusive estes comentários
+...
 def carrega_as_listas_dos_ficheiros():
-    """TODO: documentação"""
+    """
+    Esta função carrega as listas dos ficheiros
 
-    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
-    lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
-    lista_de_faturas = le_de_ficheiro(nome_ficheiro_lista_de_faturas)
-
-    return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
-
-def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
-    """TODO: documentação
-
-    :param lista_de_clientes:
-    :param lista_de_veiculos:
-    :param lista_de_faturas:
+    Return: lista de livros, lista de leitores, lista de emprestimos e lista de funcionrios
     """
 
-    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
+    lista_de_livros = le_de_ficheiro(nome_ficheiro_lista_de_livros)
+    lista_de_leitores = le_de_ficheiro(nome_ficheiro_lista_de_leitores)
+    lista_de_emprestimos = le_de_ficheiro(nome_ficheiro_lista_de_emprestimos)
+    lista_de_funcionarios = le_de_ficheiro(nome_ficheiro_lista_de_funcionarios)
+
+    return  lista_de_livros, lista_de_leitores, lista_de_emprestimos, lista_de_funcionarios
+
+def guarda_as_listas_em_ficheiros(lista_de_livros, lista_de_leitores, lista_de_emprestimos, lista_de_funcionarios):
+    """
+    guarda as listas em ficheiros
+
+    :param lista_de_livros:
+    :param lista_de_leitores:
+    :param lista_de_emprestimos:
+    :param lista_de_funcionarios:
+    """op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
     if op in ['s', 'S']:
-        guarda_em_ficheiro(nome_ficheiro_lista_de_veiculos, lista_de_veiculos)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_clientes, lista_de_clientes)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_faturas, lista_de_faturas)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_livros, lista_de_livros)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_leitores, lista_de_leitores)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_emprestimos, lista_de_emprestimos)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_funcionarios, lista_de_funcionarios)
     else:
         print("Gravação cancelada...")
 
-def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
-    """TODO: documentação
+def guarda_em_ficheiro(nome_do_ficheiro, dados):
+    """Guarda os dados recebidos num ficheiro
 
-    :param lista_de_clientes:
-    :param lista_de_veiculos:
-    :param lista_de_faturas:
+    :param nome_do_ficheiro: nome do ficheiro onde vai guardar os dados
+    :param dados: dados a serem guardados
     """
 
-    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
-    if op in ['s', 'S']:
-        guarda_em_ficheiro(nome_ficheiro_lista_de_veiculos, lista_de_veiculos)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_clientes, lista_de_clientes)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_faturas, lista_de_faturas)
-    else:
-        print("Gravação cancelada...")
+    with open(nome_do_ficheiro, "wb") as f:
+        pickle.dump(dados, f)
 
 def le_de_ficheiro(nome_ficheiro):
     """Lê os dados de um ficheiro
@@ -62,5 +59,9 @@ def le_de_ficheiro(nome_ficheiro):
     :return: o que leu do ficheiro (depende dos dados guardados)
     """
 
-    with open(nome_ficheiro, "rb") as f:
-        return pickle.load(f)
+    try:
+        with open(nome_ficheiro, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        print(f"O ficheiro {nome_ficheiro} não existe.")
+        return []
