a5addfd2b8ce2eae1b44a271f25cecf2ed8215af
a85277 <148544600+a85277@users.noreply.github.com>
2025-01-20 19:08:59 +0000

    Update and rename veiculos.py to leitores.py

---
+++ src/leitores.py
@@ -0,0 +1,29 @@
+from io_terminal import imprime_lista
+
+nome_ficheiro_lista_de_leitores = "lista_de_leitores.pk"
+
+TODO: Copie para aqui o código de cada uma das funções nos
+ficheiros com o nome leitores-*.py na pasta temp e faça um commit de cada vez
+Quando este ficheiro estiver completo com todas as suas funções,
+deve ser o unico ficheiro leitores.py existente, deve apagar
+todos os outros ficheiros temp/leitores-*.py, e inclusive estes comentários
+...
+def cria_novo_leitor():
+    """Pedir os dados de um novo leitor
+
+    :return: dicionario com o novo leitor, {"nome": <<nome>>, "nif": <<nif>>, ...}
+    """
+
+
+
+    pass
+
+
+def imprime_lista_de_leitores(lista_de_leitores):
+    '''
+    Imprimir lista e leitores
+
+    return: lista de leitores
+
+    '''
+    pass
